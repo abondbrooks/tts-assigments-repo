893a9c12934a3011ce11bc73521c3a7184a97ba8
itsjustnae <abondbrooks@gmail.com>
2019-08-20 22:30:49 -0400

    files modified and uploaded

---
+++ Notes_Labs_HW/016 JavaScript Libraries/jslibraries-lab2/js/mathmethods.js
@@ -0,0 +1,8 @@
+let base10Log = math.log(1000000, 10);
+document.getElementById("base10Log").innerHTML = base10Log;
+
+let base2Log = math.log(64, 2);
+document.getElementById("base2Log").innerHTML = base2Log;
+
+let base4Log = math.log(16384, 4);
+document.getElementById("base4Log").innerHTML = base4Log;

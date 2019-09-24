5b719ebfebf5bcbedb37c773740cbf3edf5db1d1
itsjustnae <abondbrooks@gmail.com>
2019-09-23 21:41:34 -0400

    files uploaded

--- dashboardproject/dashboard-app/src/app/product.ts
+++
@@ -1,9 +0,0 @@
-export class Product{
-  id: number;
-  productName:string;
-  category: string;
-  fullPrice: number;
-  salePrice: number;
-  availability:boolean;
-  supplier:any;
-}
--- dashboardproject/dashboard-portal/src/main/java/com/dashboard/dashboardportal/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dashboard.dashboardportal.repositories;
-
-import com.dashboard.dashboardportal.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
